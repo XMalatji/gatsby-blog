c669b001184d42c5bd2ea2c4119d340349077014
Phaseka.Malatji <Phaseka.Malatji@investec.co.za>
2018-11-09 15:35:27 +0200

    Added static query and graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,32 @@
 import React from "react"
+import { StaticQuery, graphql } from "gatsby";
 
+const Header = () => {
+    return (
+        <StaticQuery
+            query={
+                graphql`
+                    query{
+                        site{
+                            siteMetadata{
+                                title
+                                description
+                            }
+                        }
+                    }    
+                `
+            }
+            render= { data => <div>{
+                data.site.siteMetadata.title
+            } </div>}
+        />
+    )
+}
 const Layout = () => {
     return (
-        <div> Hi julle </div>
+        <div>
+            <Header />
+        </div>
     )
 }
 
